41df15ecc0eccddc7e9d8ace5a2d50e5cf42f987
midoshin712 <midoshin712@gmail.com>
2017-09-07 09:04:58 +0900

    add weather.php & watch.php

---
+++ bot/weather.php
@@ -0,0 +1,33 @@
+<?php
+
+$locations = array(
+    '滋賀県' => 250010,
+    '大阪府' => 270000,
+    '京都府' => 260010,
+    '兵庫県' => 280010,
+    '和歌山県' => 300010,
+    '奈良県' => 280010,
+    '三重県' => 240010);
+    $text = $_POST['name'];
+    $location_code;
+    if( array_key_exists($text, $locations)){
+        $location_code = $locations[$text];
+    }
+
+    $url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=" . $location_code;
+    $json = file_get_contents($url, true);
+    $json = json_decode($json, true);
+    $title = $json['title'];
+    $description = $json['description']['text'];
+    $telop_today = $json['forecasts'][0]['telop'];
+    $telop_tomorrow = $json['forecasts'][1]['telop'];
+    $name = "weather_kinki";
+    $content = "今日の天気..." . $telop_today . "<br>" . "明日の天気は... ". $telop_tomorrow . "<br>" . $descripetion . "<br>";
+    echo $content;
+    echo "{ 'name' : '" . $name . "', 'content' : '" . $content . "' }";
+    ?>
+     <?php
+    // echo "<pre>";
+    // var_dump($json);
+    // echo "</pre>";
+    ?>
